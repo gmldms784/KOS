966c33a1b72358b420df03f02832e3678a88b030
heeeun <gmldms784@naver.com>
2021-02-19 11:38:52 +0900

    make invite window

--- frontend/src/components/Sub/InviteWindow.tsx
+++ frontend/src/components/Sub/InviteWindow.tsx
@@ -1,8 +1,15 @@
 import React, {
 	Dispatch, createContext, useState, useContext, useEffect
 } from 'react';
+import clsx from 'clsx';
 
-import { Window, WindowHeader } from '../Shared';
+import {
+	TextField, Grid, Avatar, Paper
+} from '@material-ui/core';
+
+import { UserObj } from '../Model';
+import { Window, WindowHeader, Button } from '../Shared';
+import { returnIcon } from '../../function/Icon';
 
 const inviteOpenContext = createContext<number>(-1);
 const inviteDispatchContext = createContext<Dispatch<number>>(() => {});
@@ -17,8 +24,11 @@ export const InviteWindow = ({ children } : childrenObj) => {
 	// 최상위로 올릴만 한 window는 아님
 
 	// 초대할 프로젝트의 id도 받아야하기 때문에, pid가 0보다 크면 window가 열리도록 설정
-	const [pid, setPID] = useState(-1);
-	const [open, setOpen] = useState(false);
+	const [pid, setPID] = useState<number>(-1);
+	const [open, setOpen] = useState<boolean>(false);
+	const [search, setSearch] = useState<string>('');
+	const [result, setResult] = useState<Array<UserObj>>([]);
+	// todo : user.go에서 id를 주면 result로 user 정보 받아오는 api 추가 => 현재 기존 api가 token으로 바뀌어있음
 
 	useEffect(() => {
 		if (pid < 0) {
@@ -27,6 +37,27 @@ export const InviteWindow = ({ children } : childrenObj) => {
 			setOpen(true);
 		}
 	}, [pid]);
+
+	const searchIdEmail = () => {
+		console.log(`${search}검색`);
+		setResult([{
+			ID: 12314,
+			Name: 'wooheejin',
+			Icon: 'pet',
+			GitID: 'sdjksfjl@gmail.com'
+		}, {
+			ID: 12314,
+			Name: 'wooheejin',
+			Icon: 'apple',
+			GitID: 'sdjksfjl@gmail.com'
+		}, {
+			ID: 12314,
+			Name: 'wooheejin',
+			Icon: 'audio',
+			GitID: 'sdjksfjl@gmail.com'
+		}]);
+	};
+
 	return (
 		<>
 			<inviteOpenContext.Provider value={pid}>
@@ -44,7 +75,46 @@ export const InviteWindow = ({ children } : childrenObj) => {
 					mainTitle="Invite Member"
 					subTitle="프로젝트에 멤버를 초대해보세요."
 				/>
-
+				<Grid className="search-input-con">
+					<TextField
+						label="초대할 사람의 ID나 EMAIL을 검색해보세요."
+						InputLabelProps={{
+							shrink: true,
+						}}
+						variant="outlined"
+						className="search-input"
+						autoFocus={true}
+						value={search}
+						onChange={(e) => setSearch(e.target.value)}
+					/>
+					<Button
+						classList={[]}
+						value="검색하기"
+						onClickFun={searchIdEmail}
+					/>
+				</Grid>
+				<Grid className="search-result-con">
+					{
+						result.length !== 0 &&
+						result.map((user) => {
+							const a = 1;
+							return (
+								<Paper className="member-paper" elevation={3} key={user.ID}>
+									<Avatar className={clsx('member', user.Icon)}>
+										{returnIcon(user.Icon)}
+									</Avatar>
+									<Grid className="name">
+										{user.Name}
+									</Grid>
+									<Button
+										classList={[]}
+										value="초대하기"
+									/>
+								</Paper>
+							);
+						})
+					}
+				</Grid>
 			</Window>
 		</>
 	);
